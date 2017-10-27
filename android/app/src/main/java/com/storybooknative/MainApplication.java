d7ea7872c6518b8357c52fc40b48340ad17f8e90
Filipe Pinheiro <me@fampinheiro.com>
2017-10-27 13:59:29 +0100

    chore: link react-native-linear-gradient

--- android/app/src/main/java/com/storybooknative/MainApplication.java
+++ android/app/src/main/java/com/storybooknative/MainApplication.java
@@ -3,6 +3,7 @@ package com.storybooknative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.stetho.Stetho;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new SvgPackage(), new BrightcovePackage()
       );
     }
