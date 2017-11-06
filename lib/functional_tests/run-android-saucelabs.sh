438ef5083a949487ac0d269cfe6f07a0b689269b
Kouros Aliabadi <kaliabadi@qaworks.com>
2017-11-06 17:55:22 +0000

    hacking away at the hacky hack

--- lib/functional_tests/run-android-saucelabs.sh
+++ lib/functional_tests/run-android-saucelabs.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+ngrok http 7811 > /dev/null &
+export SERVER_URL=`curl -sb -H "Accept: application/json" http://127.0.0.1:4040/api/tunnels | jq '.tunnels | .[0] | .public_url'`
+
 npm run fetch-fonts
 npx rnstl --searchDir ./packages --pattern './*/*.fructose.android.js' --outputFile ./components.js
 
-yarn react-native run-android --variant=release
+# cd android && SERVER_URL=${SERVER_URL} ./gradlew assembleRelease
+cd android && ./gradlew assembleRelease && cd ..
 
 curl -u tnlweb:32571499-872a-4f5f-8b22-5dd1c2184049\
     -X POST \
     -H "Content-Type: application/octet-stream"\
     https://saucelabs.com/rest/v1/storage/tnlweb/fructose.apk?overwrite=true\
-    --data-binary @/Users/mattlowry/dev/news/times-components/android/app/build/outputs/apk/release/app-release.apk
+    --data-binary @${PWD}/android/app/build/outputs/apk/release/app-release.apk
+
+kill -9 $(lsof -ti :8081)
 
 npx compile-tests
 jest .fructose/components.test.js --setupTestFrameworkScriptFile ./.fructose/setup.saucelabs.android.js --forceExit --verbose
--- lib/functional_tests/run-android.sh
+++ lib/functional_tests/run-android.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# ngrok http 7811 > /dev/null &
+# export SERVER_URL=`curl -sb -H "Accept: application/json" http://127.0.0.1:4040/api/tunnels | jq '.tunnels | .[0] | .public_url'`
+node ./node_modules/@times-components/fructose/packages/test-helpers/src/saveServerUrl.js
+
 /Users/kourosaliabadi/Library/Android/sdk/tools/emulator @NEXUS_5X_API_22 -no-boot-anim &
 EMU_PID=$!
 adb wait-for-device
 
-yarn react-native run-android
+# SERVER_URL=${SERVER_URL} react-native run-android --variant=release
+react-native run-android --variant=release
 
 kill -9 $(lsof -ti :8081)
 
@@ -21,3 +26,4 @@ jest .fructose/components.test.js --setupTestFrameworkScriptFile ./.fructose/set
 
 kill -9 $APPIUM_PID
 kill -9 $EMU_PID
+killall ngrok
