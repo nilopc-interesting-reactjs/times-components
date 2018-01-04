b7620080470a80bcc9f61841aa71aca64f0e387b
Bernie Sumption <bernie@berniecode.com>
2018-01-04 14:06:35 +0000

    Work in progress, see comment
    In progress work to replace watermark SVG with linked static asset.
    Publishing this branch for safe keeping on 4 Jan 2018. If it’s still
    here in March you can delete it :)

--- packages/ad/ad-watermark.js
+++ packages/ad/ad-watermark.js
@@ -1,50 +1,54 @@
 import React from "react";
+import { View, Image } from "react-native";
 import PropTypes from "prop-types";
-import Watermark from "@times-components/watermark";
 
-const calculateViewBox = (width, height) => {
+const calculateImageLayout = (width, height) => {
+  const naturalWidth = 830;
+  const naturalHeight = 300;
+  const layout = (left, top, scale) => ({
+    left,
+    top,
+    width: naturalWidth * scale,
+    height: naturalHeight * scale
+  });
+
   if (width >= 970 && height >= 250) {
-    return {
-      marginLeft: 205,
-      marginTop: -40,
-      svgWidth: 584,
-      svgHeight: 220
-    };
+    return layout(390, -45, 1.13);
   }
 
   if (width >= 728 && height >= 90) {
-    return {
-      marginLeft: 630,
-      marginTop: -120,
-      svgWidth: 1200,
-      svgHeight: 50
-    };
+    return layout(381, -46, 0.615);
   }
 
   if (width >= 300 && height >= 250) {
-    // MPU
-    return {
-      marginLeft: 15,
-      marginTop: 0,
-      svgWidth: 269,
-      svgHeight: 250
-    };
+    return layout(30, 0, 1);
   }
 
-  return {
-    marginLeft: 50,
-    marginTop: 0,
-    svgWidth: width,
-    svgHeight: height
-  };
+  return layout(50, 0, 1);
 };
 
+
 const AdWatermark = ({ width, height }) => {
-  const box = calculateViewBox(width, height);
-  const viewBox = `${-box.marginLeft} ${-box.marginTop} ${box.svgWidth} ${
-    box.svgHeight
-  }`;
-  return <Watermark width={width} height={height} viewBox={viewBox} />;
+  const l = calculateImageLayout(width, height);
+
+  return (
+    <View>
+      <Image
+        source={{uri:'http://localhost:8080/watermark.svg'}}
+        width={width}
+        height={height}
+      />
+    </View>
+    // <div
+    //   style={{
+    //     width,
+    //     height,
+    //     background: 'url(http://localhost:8080/watermark.svg) no-repeat',
+    //     backgroundPosition: `${l.left}px ${l.top}px`,
+    //     backgroundSize: `${l.width}px ${l.height}px`,
+    //   }}
+    // />
+  );
 };
 
 AdWatermark.propTypes = {
