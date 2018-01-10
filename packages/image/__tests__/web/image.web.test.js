4b21f2fd11a3a284d1c50ef419f0b805bc1a6752
Bernie Sumption <bernie@berniecode.com>
2018-01-10 09:23:09 +0000

    Work in progress

--- packages/image/__tests__/web/image.web.test.js
+++ packages/image/__tests__/web/image.web.test.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 
 import React from "react";
+import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
 import Image from "../../image";
 
@@ -9,6 +10,18 @@ it("renders correctly", () => {
     .create(<Image uri="http://example.com/image.jpg" aspectRatio={3 / 2} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it("waits until a new image has loaded before removing the old", () => {
+  const component = shallow(<Image uri="http://example.com/image1.jpg" aspectRatio={3 / 2} />);
+
+  expect(component.dive("img")).toMatchSnapshot();
+
+  component.setProps({uri: "http://example.com/image2.jpg"});
+
+  console.log(component);
+
+  expect(component).toMatchSnapshot();
 });
 
 it("allows styling", () => {
--- packages/image/image.web.js
+++ packages/image/image.web.js
@@ -1,38 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 import { View } from "react-native";
 import Placeholder from "./placeholder";
 import { defaultProps, propTypes } from "./image-prop-types";
 
-const TimesImage = ({ uri, aspectRatio, style }) => {
-  const styles = {
-    wrapper: {
-      height: 0,
-      overflow: "hidden",
-      paddingBottom: `${100 / aspectRatio}%`,
-      display: "table"
-    },
-    placeholder: {
-      position: "absolute",
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      zIndex: 0
-    },
-    img: { display: "block", width: "100%", zIndex: 1, position: "absolute" }
-  };
+class TimesImage extends Component {
 
-  const boundedImg = (
-    <div style={styles.wrapper}>
-      <img src={uri} style={styles.img} alt="" />
-      <Placeholder style={styles.placeholder} />
-    </div>
-  );
+  state = {
+    imageUri: null,
+    loadingImageUri: null
+  }
 
-  // divs cannot be styled with the output of Stylesheet.create()
-  // only react native Views accept those ids
-  return style ? <View style={style}>{boundedImg}</View> : boundedImg;
-};
+  componentWillMount(props) {
+    this.updateImageSrc(this.props.uri);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.updateImageSrc(nextProps.uri);
+  }
+
+  updateImageSrc(uri) {
+    this.setState({ loadingImageUri: uri });
+  }
+
+  handleImageLoad = () => {
+    this.setState({
+      imageUri: this.state.loadingImageUri,
+      loadingImageUri: null
+    });
+  }
+
+  render() {
+    const { aspectRatio, style } = this.props;
+    const { uri } = this.state;
+    const styles = {
+      wrapper: {
+        height: 0,
+        overflow: "hidden",
+        paddingBottom: `${100 / aspectRatio}%`,
+        display: "table"
+      },
+      placeholder: {
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 0
+      },
+      img: { display: "block", width: "100%", zIndex: 1, position: "absolute" }
+    };
+
+    const boundedImg = (
+      <div style={styles.wrapper}>
+        {
+          this.state.imageUri && <img src={this.state.imageUri} style={styles.img} alt="" />
+        }
+        {
+          this.state.loadingImageUri && <img onLoad={this.handleImageLoad} src={this.state.loadingImageUri} style={styles.img} alt="" />
+        }
+        <Placeholder style={styles.placeholder} />
+      </div>
+    );
+
+    // divs cannot be styled with the output of Stylesheet.create()
+    // only react native Views accept those ids
+    return style ? <View style={style}>{boundedImg}</View> : boundedImg;
+  }
+}
 
 TimesImage.defaultProps = defaultProps;
 
